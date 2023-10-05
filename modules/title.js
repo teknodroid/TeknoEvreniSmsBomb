134ea9e6a0f1667ea62a5c7f5ff841ecb1896293
TEKNODROİD <123983520+teknodroid@users.noreply.github.com>
2023-10-05 21:18:40 +0300

    Update title.js

--- modules/title.js
+++ modules/title.js
@@ -2,11 +2,11 @@ function console_title(x) {
 
     if (process.platform == 'win32') {
 
-        process.title = x + " / FastBomber v1.0 - github.com/fastuptime";
+        process.title = x + " / TeknoEvreniSmsBomb v1.0 - github.com/teknodroid";
 
     } else {
 
-        process.stdout.write('\x1b]2;' + x + " / FastBomber v1.0 - github.com/fastuptime" + '\x1b\x5c');
+        process.stdout.write('\x1b]2;' + x + " / TeknoEvreniSmsBomb v1.0 - github.com/teknodroid" + '\x1b\x5c');
 
     }
 
